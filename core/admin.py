795b7cb806e50220d3f5175407e6568efa9d55e4
shadowarchil <shadowarchili@yahoo.com>
2023-03-31 15:37:55 +0400

    python108-final-project-main

--- core/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from core.models import Question, Answer, Vote, Tag
-from users.models import User
-
-# Register your models here.
-admin.site.register([Question, Answer, Vote, Tag, User])
